5e6c0417f0154905d990795fecec6c286a4c3445
Chris Watkins <watkins@hungry.com>
2025-08-05 00:43:25 -0600

    main

--- src/main/java/jace/hardware/CardSSC.java
+++ src/main/java/jace/hardware/CardSSC.java
@@ -18,15 +18,6 @@
  */
 package jace.hardware;
 
-import jace.EmulatorUILogic;
-import jace.config.ConfigurableField;
-import jace.config.Name;
-import jace.config.Reconfigurable;
-import jace.core.Card;
-import jace.core.Computer;
-import jace.core.RAMEvent;
-import jace.core.RAMEvent.TYPE;
-import jace.core.Utility;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -36,6 +27,16 @@ import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import jace.EmulatorUILogic;
+import jace.config.ConfigurableField;
+import jace.config.Name;
+import jace.config.Reconfigurable;
+import jace.core.Card;
+import jace.core.Computer;
+import jace.core.RAMEvent;
+import jace.core.RAMEvent.TYPE;
+import jace.core.Utility;
 import javafx.scene.control.Label;
 
 /**
@@ -48,7 +49,8 @@ import javafx.scene.control.Label;
 public class CardSSC extends Card implements Reconfigurable {
 
     @ConfigurableField(name = "TCP/IP Port", shortName = "port")
-    public short IP_PORT = 1977;
+    public short IP_PORT = (short) (1977 + getSlot());
+    /*public short IP_PORT = 1977;*/
     protected ServerSocket socket;
     protected Socket clientSocket;
     protected BufferedReader socketInput;
